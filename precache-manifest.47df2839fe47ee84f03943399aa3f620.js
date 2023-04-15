054984ee9d15cf377926fe3c607b056521d3d930
Dane Payba <dpayba01@gmail.com>
2023-04-15 14:59:45 -0700

    Updates

---
+++ precache-manifest.47df2839fe47ee84f03943399aa3f620.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f553b4682b808cbad829f709583176f",
+    "url": "/rika-concert/index.html"
+  },
+  {
+    "revision": "104a514ed3486a41678a",
+    "url": "/rika-concert/static/css/main.ee5960e6.chunk.css"
+  },
+  {
+    "revision": "fa3e07ecc97b4467053c",
+    "url": "/rika-concert/static/js/2.2c8bc25c.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/rika-concert/static/js/2.2c8bc25c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "104a514ed3486a41678a",
+    "url": "/rika-concert/static/js/main.9feddd28.chunk.js"
+  },
+  {
+    "revision": "33749c275d5528b66d63",
+    "url": "/rika-concert/static/js/runtime-main.51eac006.js"
+  }
+]);
